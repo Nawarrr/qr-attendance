3e723b9e83707a4a70fc88b18630c1802f043420
Nawar <abdonawar80@gmail.com>
2022-01-14 15:26:24 +0200

    Homepage Form

---
+++ forms.py
@@ -0,0 +1,10 @@
+from fastapi import Form
+from pydantic import BaseModel
+
+# class InstructorForm(BaseModel):
+#     course_name : str = Form(...) 
+#     instructor_name : str = Form(...)
+
+# class StudentForm(BaseModel):
+#     name : str = Form(...)
+#     email : str = Form(...)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI , Request
+from fastapi import FastAPI , Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import forms 
 
 app = FastAPI()
 
@@ -7,7 +9,24 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
-@app.route('/')
-def smth(request : Request):
+@app.get('/' , response_class=HTMLResponse)
+def home(request : Request):
     return templates.TemplateResponse("home.html" , {"request" : request})
 
+@app.post('/submitform')
+async def handle_form(course_name : str = Form(...) , instructor_name : str = Form(...)):
+    print(course_name)
+
+
+    
+
+
+# @app.get('/session' , response_model=forms.InstructorForm)
+# def session():
+#     course_name = 'course_1'
+#     instructor = "instructor_1"
+#     return templates.TemplateResponse("session.html" , {"request" : request , "course_name" : course_name , 'instructor_name' : instructor})
+
+# @app.post("/student")
+# async def student_form(request : forms.StudentForm):
+#     print(request)
